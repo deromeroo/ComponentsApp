dd9081145c8fce337b28d1483f050fcb6dff4769
Diego Romero <deromero707@gmail.com>
2024-05-22 21:59:17 -0400

    :construction: [ADD] FadeInTopBounce Animation

--- src/presentation/screens/animations/Animation101Screen.tsx
+++ src/presentation/screens/animations/Animation101Screen.tsx
@@ -1,25 +1,35 @@
 
-import { Animated, Pressable, StyleSheet, Text, View } from "react-native"
+import { Animated, Easing, Pressable, StyleSheet, Text, View } from "react-native"
 import { colors } from "../../../config/theme/theme"
 import { useRef } from "react"
 
 export const Animation101Screen = () => {
 
   const animatedOpacity = useRef( new Animated.Value(0) ).current
+  const animatedTop = useRef( new Animated.Value(-100) ).current
 
   const fadeIn = () => {
+
+    Animated.timing(animatedTop, {
+      toValue: 0,
+      duration: 700,
+      useNativeDriver: true,
+      easing: Easing.bounce
+    }).start( () => console.log('Animation Ended'))
+
     Animated.timing( animatedOpacity, {
       toValue: 1,
       duration: 300,
       useNativeDriver: true
     }).start( () => console.log('Animation ended'))
+
   }
   const fadeOut = () => {
     Animated.timing( animatedOpacity, {
       toValue: 0,
       duration: 300,
       useNativeDriver: true
-    }).start( () => console.log('Animation ended'))
+    }).start( () => animatedTop.resetAnimation() )
   }
 
   return (
@@ -27,16 +37,19 @@ export const Animation101Screen = () => {
         <Animated.View style={[
             styles.purpleBox,
             {
-              opacity: animatedOpacity
+              opacity: animatedOpacity,
+              transform: [{
+                translateY: animatedTop
+              }]
             }
         ]} />
 
         <Pressable onPress={ fadeIn } style={{ marginTop: 10 }}>
-            <Text>FadeIn</Text>
+            <Text style={styles.text}>FadeIn</Text>
         </Pressable>
 
         <Pressable onPress={ fadeOut } style={{ marginTop: 10 }}>
-            <Text>FadeOut</Text>
+            <Text style={styles.text}>FadeOut</Text>
         </Pressable>
     </View>
   )
@@ -53,5 +66,8 @@ const styles = StyleSheet.create({
         backgroundColor: colors.primary,
         width: 150,
         height: 150,
+    },
+    text: {
+      color: colors.primary
     }
 })
