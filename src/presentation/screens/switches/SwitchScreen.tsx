6237462e32c6050ac3249692f07d5a3e198912c3
Diego Romero <deromero707@gmail.com>
2024-05-24 13:11:58 -0400

    :lipstick: [ADD] CustomSwitch Component

--- src/presentation/screens/switches/SwitchScreen.tsx
+++ src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,13 +1,42 @@
-import { CustomView } from "../../components/ui/CustomView"
+import { useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context"
+
+import { CustomView } from "../../components/ui/CustomView"
+import { Card } from "../../components/ui/Card";
+import { CustomSwitch } from "../../components/ui/CustomSwitch";
 
 export const SwitchScreen = () => {
 
   const { top } = useSafeAreaInsets()
 
+  const [ state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true
+  })
+
+
   return (
     <CustomView style={{ marginTop: top + 10, paddingHorizontal: 10}}>
+      <Card>
+        <CustomSwitch 
+          isOn={ state.isActive }
+          onChange={ (value) => setState({...state, isActive: value}) }
+          text="On?"
+        />
 
+        <CustomSwitch 
+          isOn={ state.isHungry }
+          onChange={ (value) => setState({...state, isHungry: value}) }
+          text="Hungry?"
+        />
+
+        <CustomSwitch 
+          isOn={ state.isHappy }
+          onChange={ (value) => setState({...state, isHappy: value}) }
+          text="Happy?"
+        />
+      </Card>
     </CustomView>
   )
 }
