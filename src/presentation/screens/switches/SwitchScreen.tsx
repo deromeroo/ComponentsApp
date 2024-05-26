7d2401b2f5a0c402a1674071169932a8bb4b3614
Diego Romero <deromero707@gmail.com>
2024-05-25 20:07:10 -0400

    :lipstick: [ADD] Separator component

--- src/presentation/screens/switches/SwitchScreen.tsx
+++ src/presentation/screens/switches/SwitchScreen.tsx
@@ -4,6 +4,7 @@ import { useSafeAreaInsets } from "react-native-safe-area-context"
 import { CustomView } from "../../components/ui/CustomView"
 import { Card } from "../../components/ui/Card";
 import { CustomSwitch } from "../../components/ui/CustomSwitch";
+import { Separator } from "../../components/ui/Separator";
 
 export const SwitchScreen = () => {
 
@@ -25,11 +26,15 @@ export const SwitchScreen = () => {
           text="On?"
         />
 
+        <Separator />
+
         <CustomSwitch 
           isOn={ state.isHungry }
           onChange={ (value) => setState({...state, isHungry: value}) }
           text="Hungry?"
         />
+
+        <Separator />
 
         <CustomSwitch 
           isOn={ state.isHappy }
