afe82817ade5e673a305c3ca282e2eaedcb3422f
Diego Romero <deromero707@gmail.com>
2024-05-28 19:41:34 -0400

    :lipstick: KeyboardScroll

--- src/presentation/screens/inputs/TextInputScreen.tsx
+++ src/presentation/screens/inputs/TextInputScreen.tsx
@@ -1,10 +1,11 @@
-import { Text, TextInput } from 'react-native'
+import { KeyboardAvoidingView, Platform, Text, TextInput } from 'react-native'
 import { Card } from '../../components/ui/Card'
 import { CustomView } from '../../components/ui/CustomView'
 import { Title } from '../../components/ui/Title'
-import { globalStyles } from '../../../config/theme/theme'
+import { colors, globalStyles } from '../../../config/theme/theme'
 import { useState } from 'react'
 import { View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 
 export const TextInputScreen = () => {
 
@@ -16,44 +17,66 @@ export const TextInputScreen = () => {
 
 
   return (
-    <CustomView margin >
-        <Title 
-            text='Text Inputs' safe
-        />
+    <KeyboardAvoidingView
+        behavior={ Platform.OS === 'ios' ? 'padding' : undefined }
+    >
+        <ScrollView>
+            <CustomView margin >
+                <Title 
+                    text='Text Inputs' safe
+                />
 
-        <Card>
-            <TextInput 
-                style={ globalStyles.input }
-                placeholder='Full Name'
-                autoCapitalize='words'
-                autoCorrect={ false }
-                onChangeText={ value => setForm({...form, name: value}) }
-            />
+                <Card>
+                    <TextInput 
+                        style={ globalStyles.input }
+                        placeholder='Full Name'
+                        placeholderTextColor={ '#bcbcbc'}
+                        autoCapitalize='words'
+                        autoCorrect={ false }
+                        onChangeText={ value => setForm({...form, name: value}) }
+                    />
 
-            <TextInput 
-                style={ globalStyles.input }
-                placeholder='Email'
-                autoCapitalize='none'
-                autoCorrect={ false }
-                keyboardType='email-address'
-                onChangeText={ value => setForm({...form, email: value}) }
-            />
+                    <TextInput 
+                        style={ globalStyles.input }
+                        placeholder='Email'
+                        placeholderTextColor={ '#bcbcbc'}
+                        autoCapitalize='none'
+                        autoCorrect={ false }
+                        keyboardType='email-address'
+                        onChangeText={ value => setForm({...form, email: value}) }
+                    />
 
-            <TextInput 
-                style={ globalStyles.input }
-                placeholder='Phone'
-                autoCapitalize='none'
-                autoCorrect={ false }
-                keyboardType='phone-pad'
-                onChangeText={ value => setForm({...form, phone: value}) }
-            />
-        </Card>
+                    <TextInput 
+                        style={ globalStyles.input }
+                        placeholder='Phone'
+                        placeholderTextColor={ '#bcbcbc'}
+                        autoCapitalize='none'
+                        autoCorrect={ false }
+                        keyboardType='phone-pad'
+                        onChangeText={ value => setForm({...form, phone: value}) }
+                    />
+                </Card>
 
-        <View style={{ height: 10 }} />
+                <View style={{ height: 10 }} />
 
-        <Card>
-            <Text>{ JSON.stringify( form, null, 2) }</Text>
-        </Card>
-    </CustomView>
+                <Card>
+                    <Text style={{ color: colors.text }}>{ JSON.stringify( form, null, 2) }</Text>
+                    <Text style={{ color: colors.text }}>{ JSON.stringify( form, null, 2) }</Text>
+                    <Text style={{ color: colors.text }}>{ JSON.stringify( form, null, 2) }</Text>
+                    <Text style={{ color: colors.text }}>{ JSON.stringify( form, null, 2) }</Text>
+                    <Text style={{ color: colors.text }}>{ JSON.stringify( form, null, 2) }</Text>
+                    <Text style={{ color: colors.text }}>{ JSON.stringify( form, null, 2) }</Text>
+                    <Text style={{ color: colors.text }}>{ JSON.stringify( form, null, 2) }</Text>
+                    <Text style={{ color: colors.text }}>{ JSON.stringify( form, null, 2) }</Text>
+                    <Text style={{ color: colors.text }}>{ JSON.stringify( form, null, 2) }</Text>
+                    <Text style={{ color: colors.text }}>{ JSON.stringify( form, null, 2) }</Text>
+                    <Text style={{ color: colors.text }}>{ JSON.stringify( form, null, 2) }</Text>
+                    <Text style={{ color: colors.text }}>{ JSON.stringify( form, null, 2) }</Text>
+                </Card>
+            </CustomView>
+
+            <View style={{ height: 20 }} />
+        </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
