6a95fa685f8b75d967887e5da260b884215ba768
Diego Romero <deromero707@gmail.com>
2024-05-29 11:59:50 -0400

    :construction: [ADD] SetionList Characters

---
+++ src/presentation/components/ui/SubTitle.tsx
@@ -0,0 +1,28 @@
+import { Text } from "react-native"
+import { colors, globalStyles } from "../../../config/theme/theme"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+
+interface Props {
+    text: string
+    safe?: boolean
+    backgroundcolor?: string
+}
+
+export const SubTitle = ({ text, safe = false, backgroundcolor = colors.background }: Props) => {
+
+    const { top } = useSafeAreaInsets()
+
+  return (
+    <Text
+        style={{
+            ...globalStyles.subTitle,
+            marginTop: safe ? top : 0,
+            marginBottom: 10,
+            backgroundColor: backgroundcolor,
+            paddingVertical: 5
+        }}
+    >
+        {text}
+    </Text>
+  )
+}
